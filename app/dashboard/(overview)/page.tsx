5647eb76c429058874103a567c0d3c836f0b2b1b
Assim Addous <addous.assim@gmail.com>
2025-03-16 21:19:30 -0700

    invoices + add/edit/delete

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,9 +2,8 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton, InvoiceSkeleton, CardSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton, CardSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
  
 export default async function Page() {
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import styles from '@/app/ui/home.module.css'
 import Link from 'next/link';
